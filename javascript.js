841ae078744e50896596d6a7fc21ddc48c457119
Andrew Heckman <93686608+AndrewHeckman@users.noreply.github.com>
2024-10-31 21:54:23 -0500

    Add display function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 let num1;
 let num2;
 let operator;
+let result = 0;
+const display = document.querySelector("#display");
+const buttons = document.querySelector("#buttons");
+const clearButton = document.querySelector("#clear");
+const backspaceButton = document.querySelector("#backspace");
+
+buttons.addEventListener("click", handleClick);
 
 function add(a, b) {
   return a + b;
@@ -29,6 +36,36 @@ function operate(num1, num2, operator) {
     case "/":
       return divide(num1, num2);
     default:
+      return;
+  }
+}
+
+function handleClick(event) {
+  let button = event.target.id;
+  switch (button) {
+    case "clear":
+      clear();
+      break;
+    case "backspace":
+      backspace();
+      break;
+    case "answer":
+      addToDisplay(result);
+      break;
+    default:
+      addToDisplay(event.target.textContent);
       break;
   }
+}
+
+function addToDisplay(str) {
+  display.textContent = display.textContent + str;
+}
+
+function clear() {
+  display.textContent = "0";
+}
+
+function backspace() {
+  display.textContent = display.textContent.slice(0, -1);
 }
